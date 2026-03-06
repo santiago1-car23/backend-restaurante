0fc28d2e92fe0689f5f414f46ffdd679a2efbcfb
Sebastian Loaiza <scamiloloayza05@gmail.com>
2026-03-05 20:55:35 -0500

    modificacion carpeta core

--- apps/core/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('service-worker.js', views.service_worker, name='service_worker'),
-]
